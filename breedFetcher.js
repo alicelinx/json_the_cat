3897f7c828e95af0b1110b40101ec0cc14dc567f
Alice Lin <alicexxlin@gmail.com>
2023-06-08 22:12:30 +0000

    modify err return message

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,19 @@
 const request = require('request');
 
-const input = process.argv.slice(2);
-const requestUrl = `https://api.thecatapi.com/v1/breeds/search?q=${input[0]}`;
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    const data = JSON.parse(body);
+    // edge case: request failed
+    if (error) {
+      callback(error, null);
+    }
+    // edge case: breed not found which will return an empty array
+    if (!data.length) {
+      callback('Breed not found', null);
+    } else {
+      callback(null, data[0].description);
+    }
+  });
+};
 
-request(requestUrl, (error, response, body) => {
-  const data = JSON.parse(body);
-  // edge case: request failed
-  if (error) {
-    console.log(error);
-  }
-  // edge case: breed not found which will return an empty array
-  if (!data.length) {
-    console.log(`The requested breed "${input}" is not found`);
-  } else {
-    console.log(data[0].description);
-  }
-});
-
+module.exports = { fetchBreedDescription };
